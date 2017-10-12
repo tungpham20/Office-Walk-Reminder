9590cc0add2068946b250c1e26ae0378fafdff6f
Local <local@abc-8.local>
2017-10-12 17:04:03 -0400

    Initial Commit

---
+++ Office Walk Reminder/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Office Walk Reminder
+//
+//  Created by Local on 10/12/17.
+//  Copyright © 2017 Tung Pham. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
