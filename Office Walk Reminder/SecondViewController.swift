2769e61158ca147a24a53e1a0872835ab65d8d25
Local <local@abc-8.local>
2017-10-27 15:41:02 -0400

    Started with Table View on the Second View Controller

--- Office Walk Reminder/SecondViewController.swift
+++ Office Walk Reminder/SecondViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
-
+class SecondViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+    @IBOutlet weak var tableviewRequiredSteps: UITableView!
+    @IBOutlet weak var tableviewDailyWorkHours: UITableView!
+    @IBOutlet weak var tableviewReminderInterval: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.tableviewRequiredSteps.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
 
     override func didReceiveMemoryWarning() {
